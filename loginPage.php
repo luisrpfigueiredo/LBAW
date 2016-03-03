258b9a71c19dfd591b4a22af7f488aa1c8c9ab0e
Nutil <luis.rpfigueiredo@gmail.com>
2016-03-03 16:05:01 +0000

    Added login page .php

---
+++ loginPage.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+</head>
+
+<body>
+<div class="logo">
+</div>
+<div class="login-block">
+	<form id="validationForm" method="POST">
+
+		<div id="FormType">
+			<input type="button" id="LoginButton" value="Log In" >
+			<input type="button" id="RegisterButton" value="Register">
+		</di>
+
+		<div class="InputField">
+			<input type="text" value="" placeholder="Username" id="username" />
+		</div>
+
+		<div class="InputField">
+			<input type="text" value="" placeholder="Email" id="email" />
+		</div>
+
+		<div class="InputField">
+			<input type="password" value="" placeholder="Password" id="password" />
+		</div>
+
+		<div class="InputField">
+			<input type="password" value="" placeholder="Verify Password" id="verifyPassword" />
+		</div>
+
+		<input type="submit" id="submit" value="Log In">
+
+			<div id="message"></div>
+	</form>
+</div>
+
+</body>
+</html>
