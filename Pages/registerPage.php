0ff146f31ae5428c8d778b41e37528f794949680
vagrant <vagrant@homestead>
2016-03-06 12:17:15 +0000

    Register Page and Login page finished

--- Pages/registerPage.php
+++ Pages/registerPage.php
@@ -1,32 +1,81 @@
-<form class = "col-sm-offset-3 col-sm-6">
-    <ul class="nav nav-tabs">
-        <li class="active"><a href="#">Log In</a></li>
-        <li><a href="#">Register</a></li>
+<div class = "col-sm-8 col-sm-offset-2">
+
+    <ul class = "nav nav-tabs">
+        <li class = "active">
+            <a href = "#login" aria-controls = "login" role = "tab" data-toggle = "tab">Log In</a>
+        </li>
+        <li>
+            <a href = "#register" aria-controls = "register" role = "tab" data-toggle = "tab">Register</a>
+        </li>
     </ul>
-    <h2 class="text-center">Please Sign up</h2>
 
-    <div class = "form-group">
-        <label for = "email"></label>
-        <input type = "email" name = "email" class = "form-control" placeholder = "Email address" required = "" autofocus = "" autocomplete = "off">
-    </div>
+    <br/>
 
-    <div class = "form-group">
-        <label for = "username"></label>
-        <input type = "text" name = "username" class = "form-control" placeholder = "Username" required = "" autofocus = "" autocomplete = "off">
-    </div>
+    <div class = "tab-content">
 
-    <div class = "form-group">
-        <label for = "password"></label>
-        <input type = "password" name = "password" class = "form-control" placeholder = "Password" required = "" autocomplete = "off">
-    </div>
+        <div id = "login" role = "tabpanel" class = "tab-pane active">
+            <form class = "form-horizontal">
+                <div class = "form-group">
+                    <label for = "email" class = "col-sm-3 control-label">Email</label>
+                    <div class = "col-sm-8">
+                        <input type = "email" name = "email" class = "form-control" placeholder = "Email address" required = "" autofocus = "" autocomplete = "off">
+                    </div>
+                </div>
 
-    <div class = "form-group">
-        <label for = "passwordConfirm"></label>
-        <input type = "password" name = "passwordConfirm" class = "form-control" placeholder = "Confirm Password" required = "" autocomplete = "off">
-    </div>
+                <div class = "form-group">
+                    <label for = "password" class = "col-sm-3 control-label">Password</label>
+                    <div class = "col-sm-8">
+                        <input type = "password" name = "password" class = "form-control" placeholder = "Password" required = "" autocomplete = "off">
+                    </div>
+                </div>
 
-    <button class = "btn btn-lg btn-primary btn-block" type = "submit">Sign up</button>
-    <div class="form-group">
-        <a href="#"> Forgot Password?</a>
+                <div class = "form-group">
+                    <div class = "col-sm-3 col-sm-offset-3">
+                        <button class = "btn btn-primary" type = "submit">Log In</button>
+                    </div>
+                    <div class = "col-sm-5 text-right">
+                        <a href = "#" class = "small"> Forgot Password?</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+        <div role = "tabpanel" class = "tab-pane" id = "register">
+            <form class = "form-horizontal">
+                <div class = "form-group">
+                    <label for = "email" class = "col-sm-3 control-label">Email</label>
+                    <div class = "col-sm-8">
+                        <input type = "email" name = "email" class = "form-control" placeholder = "Email address" required = "" autofocus = "" autocomplete = "off">
+                    </div>
+                </div>
+
+                <div class = "form-group">
+                    <label for = "username" class = "col-sm-3 control-label">Username</label>
+                    <div class = "col-sm-8">
+                        <input type = "text" name = "username" class = "form-control" placeholder = "Username" required = "" autofocus = "" autocomplete = "off">
+                    </div>
+                </div>
+
+                <div class = "form-group">
+                    <label for = "password" class = "col-sm-3 control-label">Password</label>
+                    <div class = "col-sm-8">
+                        <input type = "password" name = "password" class = "form-control" placeholder = "Password" required = "" autocomplete = "off">
+                    </div>
+                </div>
+
+                <div class = "form-group">
+                    <label for = "passwordConfirm" class = "col-sm-3 control-label">Confirm Password</label>
+                    <div class = "col-sm-8">
+                        <input type = "password" name = "passwordConfirm" class = "form-control" placeholder = "Confirm Password" required = "" autocomplete = "off">
+                    </div>
+                </div>
+
+                <div class = "form-group">
+                    <div class = "col-sm-3 col-sm-offset-3">
+                        <button class = "btn btn-primary" type = "submit">Register</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-</form>
+</div>
--- Pages/template.php
+++ Pages/template.php
@@ -18,5 +18,14 @@
 
 <?= HTML::script('jquery-2.2.1.min.js') ?>
 <?= HTML::script('bootstrap.min.js') ?>
+
+<script type = "text/javascript">
+    $(document).ready(function () {
+        $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+        $('#myTabs a:first').tab('show') // Select first tab
+        $('#myTabs a:last').tab('show') // Select last tab
+        $('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+    });
+</script>
 </body>
 </html>
