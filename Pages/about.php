eda413a5fb3c54ce5675f8d228f141d4d3932887
tiagomct <tiagomctavares@gmail.com>
2016-03-18 14:10:40 +0000

    Finished About Us Page

--- Pages/about.php
+++ Pages/about.php
@@ -3,10 +3,21 @@
 
     <div class = "panel panel-default">
         <div class = "panel-heading">
-            <h3 class = "panel-title">Panel title</h3>
+            <h3 class = "panel-title">About Us</h3>
         </div>
         <div class = "panel-body">
-            Panel content
+            <p>
+                Este projeto destina-se à especificação, desenvolvimento e promoção de um sistema de informação disponível através da Web para a gestão de perguntas e as suas respetivas respostas, num ambiente colaborativo.
+            </p>
+            <p>
+                Os utilizadores conseguem por este meio partilhar o seu conhecimento, permitindo criar-se assim uma comunidade de entreajuda.
+            </p>
+            <p>
+                O sistema deve primeiramente permitir o registo e autenticação de utilizadores, assim como a anulação do registo de um utilizador. O sistema deverá também oferecer facilidades de carregamento de informação das perguntas do utilizador, o carregamento de respostas às perguntas dos utilizadores, facilidades de pesquisa por utilizador (nomeadamente listar todas as perguntas e respostas feitas por um utilizador) e funcionalidades de avaliação de perguntas e respostas (atribuir um rating à pergunta e às respostas através de votos).
+                </p>
+            <p>
+                Devem existir perfis registados de uso-comum que poderão consultar a informação e registar as suas perguntas, assim como adicionar respostas a perguntas existentes. Poderão além disso adicionar o seu voto (positivo ou negativo) a perguntas a que tenham acesso. Devem existir também perfis de administração que têm todos os privilégios de acesso. Os administradores têm privilégios de operação, privilégios de introdução de informação no sistema e de modificação de perfis de utilizadores não-administradores (remover o utilizador, banir temporariamente, promover a administrador).
+            </p>
         </div>
     </div>
 </div>
