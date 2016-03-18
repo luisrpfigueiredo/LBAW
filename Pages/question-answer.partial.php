6264457f5f798e8eda7cf24f604e8ea12a38ce03
tiagomct <tiagomctavares@gmail.com>
2016-03-18 15:42:29 +0000

    Questions Page

---
+++ Pages/question-answer.partial.php
@@ -0,0 +1,27 @@
+<div class = "col-sm-offset-1 col-sm-11 question-space  container-white">
+    <div class = "col-sm-1">
+        <?php render('vote') ?>
+    </div>
+    <div class = "col-sm-11">
+        <p class = "break-word">Im sure there is a simple solution to this problem. Basically if I have two columns how can i add a space between them.</p>
+
+
+        <div class = "statistics col-sm-12">
+                        <span>
+                            <i class = "glyphicon glyphicon-time"></i>
+                            1 min ago
+                        </span>
+            <span>
+            <i class = "glyphicon glyphicon-user"></i>
+            Gustavo Daniel
+                </span>
+        </div>
+
+        <div class="actions pull-right">
+            <i class="glyphicon glyphicon-share-alt"></i>
+            Reply
+        </div>
+
+
+    </div>
+</div>
--- Pages/question.php
+++ Pages/question.php
@@ -1,68 +1,64 @@
 <?= HTML::style("css/voting.css"); ?>
 <?= HTML::script("voting.js"); ?>
 <div class = "container">
-    <div class = "panel panel-default col-sm-10">
+    <?php render('breadcrumb') ?>
+
+    <div class = "panel panel-default">
+
         <!-- Default panel contents -->
         <div class = "panel-heading">
             <h3 class = "panel-title">Twitter bootstrap grid system. Spacing between columns</h3>
         </div>
-        <div class = "panel-body">
+        <div class = "panel-body question-details-panel no-padding-horizontal">
 
-            <p class = "break-word">Im sure there is a simple solution to this problem. Basically if I have two columns how can i add a space between them.</p>
+            <div class = "col-sm-12 container-white">
+                <div class = "col-sm-1">
+                    <?php render('vote') ?>
+                </div>
+                <div class = "col-sm-11">
+                    <p class = "break-word">Im sure there is a simple solution to this problem. Basically if I have two columns how can i add a space between them.</p>
 
-            <div class = "panel panel-default">
-                <div class = "panel-body">
-                    <i class = "glyphicon glyphicon-user"></i>
-                    João Lopes
-                    <i class = "glyphicon glyphicon-time"></i>
-                    1 min ago
+
+                    <div class = "statistics col-sm-12">
+                        <span class = "text-danger">
+                            <i class = "glyphicon glyphicon-remove"></i>
+                            Not Solved
+                        </span>
+                        <span>
+                            <i class = "glyphicon glyphicon-time"></i>
+                            1 min ago
+                        </span>
+                        <span class = "text-star">
+                            <i class = "glyphicon glyphicon-star"></i>
+                            6 follows
+                        </span>
+                        <span>
+                            <i class="glyphicon glyphicon-user"></i>
+                            Gustavo Daniel
+                        </span>
+                    </div>
+
+
+                    <div class="actions pull-right">
+                        <i class="glyphicon glyphicon-share-alt"></i>
+                        Reply
+                    </div>
 
                 </div>
             </div>
 
+            <?php render('question-answer') ?>
 
+            <?php render('question-answer') ?>
+
+            <?php render('question-answer') ?>
+
+            <?php render('question-answer') ?>
         </div>
 
-
     </div>
-    <div class = "col-sm-2">
-        <blockquote class = "pull-right">
-            <div class = "vote chev">
-                <div class = "increment up"></div>
-                <div class = "increment down"></div>
 
-                <div class = "count">0</div>
-        </blockquote>
+    <div class = "col-sm-12 text-center">
+        <button id = "singlebutton" name = "singlebutton" class = "btn btn-primary col-sm-6 col-sm-offset-3">Load More...</button>
     </div>
-</div>
-
-<div class = "container">
-
-    <div class = "panel panel-default col-sm-10">
-        <div class = "panel-body">
-            I think ....
-            <br><br>
-            <div class = "panel panel-default">
-                <div class = "panel-body">
-                    <i class = "glyphicon glyphicon-user"></i>
-                    Gustavo Daniel
-                    <i class = "glyphicon glyphicon-time"></i>
-                    1 min ago
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class = "col-sm-2">
-        <blockquote class = "pull-right">
-            <div class = "vote chev">
-                <div class = "increment up"></div>
-                <div class = "increment down"></div>
-
-                <div class = "count">0</div>
-        </blockquote>
-    </div>
-</div>
-
-<div class = "col-md-10 text-center">
-    <button id = "singlebutton" name = "singlebutton" class = "btn btn-primary">Load More...</button>
 </div>
