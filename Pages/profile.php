142526eaa9849126145f9f4c97c3af1eaf42140c
ricardolopes8e <ricardolopes8e@gmail.com>
2016-03-18 14:06:53 +0000

    update profile

--- Pages/profile.php
+++ Pages/profile.php
@@ -24,45 +24,8 @@
         </tr>
         <tr>
         <td>Description:</td>
-        <td>João Correia Lopes is an Assistant Professor in Informatics Engineering at the Universidade do Porto and a researcher at INESC TEC,
-
-He has graduated in Electrical Engineering in the University of Porto in 1984 and holds a PhD in Computing Science by Glasgow University in 1997.
-
-His teaching includes undergraduate and graduate courses in databases and web applications, software engineering and programming, markup languages and semantic web.
-
-He has been involved in research projects in the area of long-term preservation, service-oriented architectures and e-Science.
-Currently his main research interests are e-Science and research data  management. João Correia Lopes is an Assistant Professor in Informatics Engineering at the Universidade do Porto and a researcher at INESC TEC,
-
-He has graduated in Electrical Engineering in the University of Porto in 1984 and holds a PhD in Computing Science by Glasgow University in 1997.
-
-His teaching includes undergraduate and graduate courses in databases and web applications, software engineering and programming, markup languages and semantic web.
-
-He has been involved in research projects in the area of long-term preservation, service-oriented architectures and e-Science.
-Currently his main research interests are e-Science and research data  management. João Correia Lopes is an Assistant Professor in Informatics Engineering at the Universidade do Porto and a researcher at INESC TEC,
-
-He has graduated in Electrical Engineering in the University of Porto in 1984 and holds a PhD in Computing Science by Glasgow University in 1997.
-
-His teaching includes undergraduate and graduate courses in databases and web applications, software engineering and programming, markup languages and semantic web.
-
-He has been involved in research projects in the area of long-term preservation, service-oriented architectures and e-Science.
-Currently his main research interests are e-Science and research data  management. João Correia Lopes is an Assistant Professor in Informatics Engineering at the Universidade do Porto and a researcher at INESC TEC,
-
-He has graduated in Electrical Engineering in the University of Porto in 1984 and holds a PhD in Computing Science by Glasgow University in 1997.
-
-His teaching includes undergraduate and graduate courses in databases and web applications, software engineering and programming, markup languages and semantic web.
-
-He has been involved in research projects in the area of long-term preservation, service-oriented architectures and e-Science.
-Currently his main research interests are e-Science and research data  management. João Correia Lopes is an Assistant Professor in Informatics Engineering at the Universidade do Porto and a researcher at INESC TEC,
-
-He has graduated in Electrical Engineering in the University of Porto in 1984 and holds a PhD in Computing Science by Glasgow University in 1997.
-
-His teaching includes undergraduate and graduate courses in databases and web applications, software engineering and programming, markup languages and semantic web.
-
-He has been involved in research projects in the area of long-term preservation, service-oriented architectures and e-Science.
-Currently his main research interests are e-Science and research data  management. 
-</td>
+        <td>João Correia Lopes is an Assistant Professor in Informatics Engineering at the Universidade do Porto and a researcher at INESC TEC...</td>
         </tr>
-
       </tbody>
     </table>
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Edit profile</button>
@@ -88,9 +51,43 @@ Currently his main research interests are e-Science and research data  managemen
     <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas erat mi, et eleifend erat rutrum vitae. Suspendisse potenti.                In hac habitasse platea dictumst. Etiam velit est, aliquam eu risus ut, hendrerit volutpat ante. Donec in consequat orci. In maximus lobortis risus in porta. Vivamus blandit, felis vitae pretium faucibus, diam risus suscipit purus, ut tempor nulla libero sit amet tortor. Phasellus fermentum turpis eu lectus mattis, vel aliquam ante elementum. Suspendisse ac elit leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas erat mi, et eleifend erat rutrum vitae. Suspendisse potenti.                In hac habitasse platea dictumst. Etiam velit est, aliquam eu risus ut, hendrerit volutpat ante. Donec in consequat orci. In maximus lobortis risus in porta. Vivamus blandit, felis vitae pretium faucibus, diam risus suscipit purus, ut tempor nulla libero sit amet tortor. Phasellus fermentum turpis eu lectus mattis, vel aliquam ante elementum. Suspendisse ac elit leo.</p>
   </a>
 </div>
+<div calss="text-center">
+<button type="button" class="btn btn-primary pull-right">Load more...</button>
+</div>
 
-<button type="button" class="btn btn-primary pull-right" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">More...</button>
 
-
-
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">Settings</h4>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="control-label">New Username:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="control-label">New email:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="control-label">New password:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="control-label">Confirm password:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Edit</button>
+      </div>
+    </div>
+  </div>
+</div>
 
