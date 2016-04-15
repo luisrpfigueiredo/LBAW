e482896d2196758ec3937fd952522f99cd8fda09
tiagomct <tiagomctavares@gmail.com>
2016-04-15 16:02:58 +0000

    frmk config for gnomo

--- lbaw13/frmk/config/init.php
+++ lbaw13/frmk/config/init.php
@@ -1,17 +1,17 @@
 <?php
-  session_set_cookie_params(3600, '/~lbaw15xx'); //FIXME
+  session_set_cookie_params(3600, '/~lbaw1513');
   session_start();
 
   error_reporting(E_ERROR | E_WARNING); // E_NOTICE by default
 
-  $BASE_DIR = '/opt/lbaw/lbaw15xx/public_html/frmk/'; //FIXME
-  $BASE_URL = '/~lbaw15xx/frmk/'; //FIXME
+  $BASE_DIR = '/opt/lbaw/lbaw1513/public_html/frmk/'; 
+  $BASE_URL = '/~lbaw1513/frmk/';
 
-  $conn = new PDO('pgsql:host=dbm;dbname=lbaw15xx', 'lbaw15xx', '_password_'); //FIXME
+  $conn = new PDO('pgsql:host=dbm.fe.up.pt;dbname=lbaw1513', 'lbaw1513', 'RF77P7Q9');
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $conn->exec('SET SCHEMA \'frmk\''); //FIXME?
+  $conn->exec('SET SCHEMA \'public\'');
 
   include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
   
