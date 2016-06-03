02d1ac7e083383cd23fcd89d6920583aec23d12f
tiagomct <tiagomctavares@gmail.com>
2016-06-03 15:22:14 +0000

    Changed permissions to redirect home

--- final/pages/questions/details.php
+++ final/pages/questions/details.php
@@ -3,8 +3,15 @@ include_once('../../config/init.php');
 include_once($BASE_DIR . 'database/questions.php');
 include_once($BASE_DIR . 'database/votes.php');
 
+$user = auth_user();
+$loggedIn = false;
+if($user) {
+    $loggedIn = true;
+}
+
 $questions = questionsFromIds(array($_GET['question']));
 $question = $questions[0];
 
+$smarty->assign('logged', $loggedIn);
 $smarty->assign('question', $question);
 $smarty->display('questions/details.tpl');
