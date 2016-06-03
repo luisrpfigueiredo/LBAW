869eb6b63186800e0bdb65ffa5e0970158f2cc78
Nutil <luis.rpfigueiredo@gmail.com>
2016-06-03 21:52:46 +0100

    Fixed minor minor bug

--- final/api/admin/handleBan.php
+++ final/api/admin/handleBan.php
@@ -7,16 +7,7 @@ include_once($BASE_DIR . 'database/admin/manageUsers.php');
     $notes = $_POST['notes'];
     $expirationTimestampUnix = intval($_POST['expirationDate']);
 
-    if($expirationTimestampUnix == 0){
-        $response['error'] = 'No Expiration Date supplied';
-        echo json_encode($response);
-        exit();
-    }
-    if($expirationTimestampUnix < 0 || ($expirationTimestampUnix - time()) <= 0){
-        $response['error'] = 'The expiration date must be in the future';
-        echo json_encode($response);
-        exit();
-    }
+
 
     if($userID == null){
         $response['error'] = 'No ID supplied';
@@ -37,6 +28,16 @@ include_once($BASE_DIR . 'database/admin/manageUsers.php');
     if ($isBanned == "yes") {
         unbanUser($userID, $notes);
     } else {
+        if($expirationTimestampUnix == 0){
+            $response['error'] = 'No Expiration Date supplied';
+            echo json_encode($response);
+            exit();
+        }
+        if($expirationTimestampUnix < 0 || ($expirationTimestampUnix - time()) <= 0){
+            $response['error'] = 'The expiration date must be in the future';
+            echo json_encode($response);
+            exit();
+        }
         $expirationDate = date('Y-m-d h:i:s', $expirationTimestampUnix);
         banUser([$userID, intval($_SESSION['user']['id']), $notes, $expirationDate]);
     }
